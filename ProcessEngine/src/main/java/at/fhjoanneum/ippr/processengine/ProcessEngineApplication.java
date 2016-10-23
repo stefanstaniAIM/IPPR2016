41491b1703b1dae241f8085706fc78507aba9de6
stefa <stefa@DESKTOP-FMBDLR3>
2016-10-23 13:12:16 +0200

    initial commit of ProcessEngine

---
+++ ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/ProcessEngineApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.processengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessEngineApplication.class, args);
+	}
+}
