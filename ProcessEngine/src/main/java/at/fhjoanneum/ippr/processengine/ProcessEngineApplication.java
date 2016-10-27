550dbbe8b867b2e5ffde8c20ad9d2132dc34f244
stefa <stefa@DESKTOP-FMBDLR3>
2016-10-27 15:43:55 +0200

    Updated build

--- ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/ProcessEngineApplication.java
+++ ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/ProcessEngineApplication.java
@@ -2,11 +2,13 @@ package at.fhjoanneum.ippr.processengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("at.fhjoanneum.ippr.persistence.entities")
 public class ProcessEngineApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(ProcessEngineApplication.class, args);
 	}
 }
