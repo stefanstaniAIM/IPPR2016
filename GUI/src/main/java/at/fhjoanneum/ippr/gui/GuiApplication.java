509bcf0dd63a963fac0b890d837a40269d22120b
stefa <stefa@10.15.202.126>
2016-10-27 11:43:34 +0200

    Initial commit for gui

---
+++ GUI/src/main/java/at/fhjoanneum/ippr/gui/GuiApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiApplication.class, args);
+	}
+}
