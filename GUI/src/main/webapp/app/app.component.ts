3752e82710183faa096589fbcb3ed218060cd3bb
MG <mgeisriegler@gmx.at>
2016-10-30 11:15:27 +0100

    Add relative paths to angular

--- GUI/src/main/webapp/app/app.component.ts
+++ GUI/src/main/webapp/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   template: `
+    <navbar></navbar>
     <h1>{{title}}</h1>
     <a routerLink="/dashboard">Dashboard</a>
     <router-outlet></router-outlet>
