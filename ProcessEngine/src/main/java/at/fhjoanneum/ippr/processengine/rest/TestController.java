65208078d216a53a9b4705ae8147cb560fd5df48
stefa <stefa@10.15.201.80>
2016-11-03 16:25:53 +0100

    JWT Token

--- ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/rest/TestController.java
+++ ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/rest/TestController.java
@@ -1,16 +1,15 @@
 package at.fhjoanneum.ippr.processengine.rest;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+  // @AuthenticationPrincipal final UserDetails user
   @GetMapping("/home")
-  public String home(@AuthenticationPrincipal final UserDetails user) {
-    return "hello cool guy with the name " + user;
+  public String home() {
+    return "hello cool guy with the name "; // + user;
   }
 
 }
