44cde57eb8b7ecf4e5ae9f2d59277d3ead22c986
stefa <stefa@10.15.203.93>
2016-11-16 13:03:13 +0100

    Akka Spring integration

--- ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/test/ProcessActor.java
+++ ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/test/ProcessActor.java
@@ -2,16 +2,26 @@ package at.fhjoanneum.ippr.processengine.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import akka.actor.UntypedActor;
 import at.fhjoanneum.ippr.processengine.akka.messages.process.ProcessStartedMessage;
+import at.fhjoanneum.ippr.processengine.services.ProcessService;
 
+@Component("ProcessActor")
+@Scope("prototype")
 public class ProcessActor extends UntypedActor {
 
   private final static Logger LOG = LoggerFactory.getLogger(ProcessActor.class);
 
+  @Autowired
+  private ProcessService processService;
+
   @Override
   public void onReceive(final Object arg0) throws Throwable {
+    LOG.info("********************" + processService);
     LOG.info("Reply with ProcessStartedMessage");
     getSender().tell(new ProcessStartedMessage(2222L), getSelf());
     // getSender().tell(new akka.actor.Status.Failure(new Exception()), getSelf());
--- ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/test/ProcessSupervisorActor.java
+++ ProcessEngine/src/main/java/at/fhjoanneum/ippr/processengine/test/ProcessSupervisorActor.java
@@ -5,18 +5,26 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import akka.actor.ActorRef;
-import akka.actor.Props;
 import akka.actor.UntypedActor;
 import at.fhjoanneum.ippr.processengine.akka.AkkaSelector;
+import at.fhjoanneum.ippr.processengine.akka.config.SpringExtension;
 import at.fhjoanneum.ippr.processengine.akka.messages.process.ProcessStartMessage;
 
+@Component("ProcessSupervisorActor")
+@Scope("prototype")
 public class ProcessSupervisorActor extends UntypedActor {
 
   private final static Logger LOG = LoggerFactory.getLogger(ProcessSupervisorActor.class);
 
   private final AkkaSelector akkaSelector = new AkkaSelector();
+
+  @Autowired
+  private SpringExtension springExtension;
 
   @Override
   public void onReceive(final Object msg) throws Throwable {
@@ -29,8 +37,11 @@ public class ProcessSupervisorActor extends UntypedActor {
 
       final Optional<ActorRef> actorOpt = akkaSelector.findActor(getContext(), processInstanceId);
       if (!actorOpt.isPresent()) {
-        getContext().actorOf(Props.create(ProcessActor.class), processInstanceId).forward(msg,
+        getContext().actorOf(springExtension.props("ProcessActor"), processInstanceId).forward(msg,
             getContext());
+
+        // getContext().actorOf(Props.create(ProcessActor.class), processInstanceId).forward(msg,
+        // getContext());
       } else {
         actorOpt.get().forward(msg, getContext());
       }
