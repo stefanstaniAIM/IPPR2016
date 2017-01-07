91102c158538f19c6dfe4e17ef08e83c9b0a873a
MG <mgeisriegler@gmx.at>
2017-01-07 20:46:16 +0100

    date seconds 0 if null

--- GUI-dev/src/app/pipes/commaDate.pipe.ts
+++ GUI-dev/src/app/pipes/commaDate.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'commaDate'})
 export class CommaDatePipe implements PipeTransform {
   transform(values: string, args: string[]): Date {
-    return new Date(parseInt(values[0]), parseInt(values[1]), parseInt(values[2]), parseInt(values[3]), parseInt(values[4]), parseInt(values[5]));
+    return new Date(parseInt(values[0]) || 0, parseInt(values[1]) || 0, parseInt(values[2]) || 0, parseInt(values[3]) || 0, parseInt(values[4]) || 0, parseInt(values[5]) || 0);
   }
 }
