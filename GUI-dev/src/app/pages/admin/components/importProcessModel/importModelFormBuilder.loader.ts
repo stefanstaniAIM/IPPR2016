7817271960061e5837c988e0d97aac5a487d5159
MG <mgeisriegler@gmx.at>
2017-02-05 19:52:01 +0100

    form-builder can be used to create fields

---
+++ GUI-dev/src/app/pages/admin/components/importProcessModel/importModelFormBuilder.loader.ts
@@ -0,0 +1,5 @@
+require('formBuilder/dist/form-builder.js');
+require('formBuilder/dist/form-render.js');
+require('style-loader!formBuilder/dist/form-builder.css');
+require('style-loader!formBuilder/dist/form-render.css');
+require('jquery-ui-bundle');
--- GUI-dev/src/app/pages/admin/components/importProcessModel/importProcessModel.component.ts
+++ GUI-dev/src/app/pages/admin/components/importProcessModel/importProcessModel.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { ProcessesService } from '../../../../Processes.service';
+import './importModelFormBuilder.loader.ts';
 
 @Component({
   selector: 'import',
-  styles: [],
+  styles: [require('./importProcessModel.scss')],
   template: require('./importProcessModel.html')
 })
 export class ImportProcessModel implements OnInit {
    processModel;
    rules;
    error = undefined;
+   formBuilder;
+   buildedBusinessObjects = [];
+   currentSelectedBusinessObject;
 
   constructor(protected service:ProcessesService) {}
 
@@ -34,10 +38,15 @@ export class ImportProcessModel implements OnInit {
           data => {
              console.log(data);
              that.processModel = JSON.parse(data['_body']);
+             that.processModel.boms.forEach(businessObject => {
+               that.buildedBusinessObjects[businessObject.name] = {};
+             });
              that.initRules();
           },
           err => this.error = err,
-          () => console.log('Request Complete')
+          () => {
+            console.log('Request Complete');
+          }
         );
   }
 
@@ -48,6 +57,8 @@ export class ImportProcessModel implements OnInit {
           data => {
              console.log(data);
              that.rules = JSON.parse(data['_body']);
+             that.currentSelectedBusinessObject = that.processModel.boms[0];
+             that.initFormBuilder(that.currentSelectedBusinessObject);
           },
           err => this.error = err,
           () => console.log('Request Complete')
@@ -57,5 +68,38 @@ export class ImportProcessModel implements OnInit {
   uploadProcessModel(form):void {
     var that = this;
     console.log(form);
+  }
+
+  initFormBuilder(businessObject): void {
+    var options = {
+      dataType: 'json', // default: 'xml',
+      disableFields: ['autocomplete', 'button', 'checkbox-group', 'file', 'header', 'hidden', 'paragraph', 'select', 'text'],
+      showActionButtons: false
+    };
+    this.formBuilder = jQuery(".formBuilder").formBuilder(options).data('formBuilder');
+  }
+
+  getFormData(businessObject): void {
+    if(this.currentSelectedBusinessObject !== businessObject){
+      this.buildedBusinessObjects[this.currentSelectedBusinessObject.name] = this.formBuilder.formData;
+      var formData = this.buildedBusinessObjects[businessObject.name];
+      formData = jQuery.isEmptyObject(formData) ? undefined : formData === "[]" ? undefined : formData;
+      if(formData !== undefined){
+        //This is a hack, otherwise setData will not work correctly
+        this.formBuilder.actions.addField(
+        	{
+        		"type": "paragraph",
+        		"subtype": "p",
+        		"label": "Paragraph",
+        		"className": "paragraph"
+        	}
+        );
+        this.formBuilder.actions.setData(formData);
+      } else {
+        this.formBuilder.actions.clearFields();
+      }
+    }
+    this.currentSelectedBusinessObject = businessObject;
+    console.log(this.formBuilder.formData);
   }
 }
