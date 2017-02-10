e1166c907713f6789b637da624d46858a729fcbd
stefa <stefa@10.15.200.96>
2017-02-10 09:00:41 +0100

    Changed REST endpoint

--- Gateway/src/main/java/at/fhjoanneum/ippr/gateway/api/controller/OwlImportGatewayController.java
+++ Gateway/src/main/java/at/fhjoanneum/ippr/gateway/api/controller/OwlImportGatewayController.java
@@ -1,23 +1,18 @@
 package at.fhjoanneum.ippr.gateway.api.controller;
 
-import java.io.ByteArrayInputStream;
 import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import at.fhjoanneum.ippr.commons.dto.owlimport.jsonimport.ImportProcessModelDTO;
 import at.fhjoanneum.ippr.commons.dto.owlimport.reader.OWLProcessModelDTO;
@@ -33,10 +28,12 @@ public class OwlImportGatewayController {
   @Autowired
   private OwlImportGatewayCallerImpl owlImportGatewayCaller;
 
-  @RequestMapping(value = "api/owlprocessmodel", method = RequestMethod.GET)
+  @RequestMapping(value = "api/owlprocessmodel", method = RequestMethod.POST)
   public @ResponseBody Callable<ResponseEntity<OWLProcessModelDTO>> getOWLProcessModel(
-      final HttpServletRequest request) {
+      final @RequestBody String owlContent, final HttpServletRequest request) {
     return () -> {
+      LOG.debug("Retrieved owl content: {}", owlContent);
+      final HttpHeaderUser user = new HttpHeaderUser(request);
       return owlImportGatewayCaller.getOWLProcessModel().get();
     };
   }
@@ -49,22 +46,4 @@ public class OwlImportGatewayController {
       return owlImportGatewayCaller.importProcessModel(processModelDTO, user).get();
     };
   }
-
-  @RequestMapping(value = "api/owlupload", method = RequestMethod.POST)
-  public @ResponseBody Callable<ResponseEntity<?>> uploadOwlFile(
-      @RequestParam("uploadfile") final MultipartFile file, final HttpServletRequest request) {
-    return () -> {
-      try {
-        final ByteArrayInputStream stream = new ByteArrayInputStream(file.getBytes());
-        final String owlContent = IOUtils.toString(stream, "UTF-8");
-        LOG.debug("owl input is: {}", owlContent);
-        final HttpHeaderUser user = new HttpHeaderUser(request);
-        return new ResponseEntity<String>(owlContent, HttpStatus.OK);
-      } catch (final Exception e) {
-        LOG.error(e.getMessage());
-        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-      }
-    };
-  }
-
 }
