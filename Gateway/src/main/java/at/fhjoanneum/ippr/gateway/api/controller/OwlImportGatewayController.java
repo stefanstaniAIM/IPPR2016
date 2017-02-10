75d5d361fd8ded631a386d64236537d179702f89
stefa <stefa@10.15.200.96>
2017-02-10 08:24:31 +0100

    File upload test

--- Gateway/src/main/java/at/fhjoanneum/ippr/gateway/api/controller/OwlImportGatewayController.java
+++ Gateway/src/main/java/at/fhjoanneum/ippr/gateway/api/controller/OwlImportGatewayController.java
@@ -1,16 +1,23 @@
 package at.fhjoanneum.ippr.gateway.api.controller;
 
+import java.io.ByteArrayInputStream;
 import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import at.fhjoanneum.ippr.commons.dto.owlimport.jsonimport.ImportProcessModelDTO;
 import at.fhjoanneum.ippr.commons.dto.owlimport.reader.OWLProcessModelDTO;
@@ -20,6 +27,8 @@ import at.fhjoanneum.ippr.gateway.api.services.impl.OwlImportGatewayCallerImpl;
 @RestController
 @RequestMapping(produces = "application/json; charset=UTF-8")
 public class OwlImportGatewayController {
+
+  private final static Logger LOG = LoggerFactory.getLogger(OwlImportGatewayController.class);
 
   @Autowired
   private OwlImportGatewayCallerImpl owlImportGatewayCaller;
@@ -38,6 +47,23 @@ public class OwlImportGatewayController {
     return () -> {
       final HttpHeaderUser user = new HttpHeaderUser(request);
       return owlImportGatewayCaller.importProcessModel(processModelDTO, user).get();
+    };
+  }
+
+  @RequestMapping(value = "api/owlupload", method = RequestMethod.POST)
+  public @ResponseBody Callable<ResponseEntity<?>> uploadOwlFile(
+      @RequestParam("uploadfile") final MultipartFile file, final HttpServletRequest request) {
+    return () -> {
+      try {
+        final ByteArrayInputStream stream = new ByteArrayInputStream(file.getBytes());
+        final String owlContent = IOUtils.toString(stream, "UTF-8");
+        LOG.debug("owl input is: {}", owlContent);
+        final HttpHeaderUser user = new HttpHeaderUser(request);
+        return new ResponseEntity<String>(owlContent, HttpStatus.OK);
+      } catch (final Exception e) {
+        LOG.error(e.getMessage());
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+      }
     };
   }
 
