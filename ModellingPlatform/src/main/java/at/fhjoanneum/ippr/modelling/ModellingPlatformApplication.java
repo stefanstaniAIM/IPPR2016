8d3cd45e477bd597a49cbfe5730bf1c9f3df1c61
Stefan Stani <stefan.stani@edu.fh-joanneum.at>
2017-02-16 12:53:59 +0100

    new modelling platform

---
+++ ModellingPlatform/src/main/java/at/fhjoanneum/ippr/modelling/ModellingPlatformApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.modelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModellingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModellingPlatformApplication.class, args);
+	}
+}
