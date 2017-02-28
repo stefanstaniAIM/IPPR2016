a9b4584b24d0bc3d73e84bc54d6ef76142c427da
Stefan Stani <stefan.stani@gmail.com>
2017-02-28 13:21:44 +0100

    New service for external subjects

---
+++ ExternalCommunicator/src/main/java/at/fhjoanneum/ippr/communicator/ExternalCommunicatorApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.communicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalCommunicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalCommunicatorApplication.class, args);
+	}
+}
