6cc58c6c17f6ead2fc029dd5f7b68fb415182d22
Stefan Stani <stefan.stani@gmail.com>
2017-03-06 21:30:38 +0100

    Finished config server for external communicator

--- ConfigurationService/src/main/java/at/fhjoanneum/ippr/configservice/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/at/fhjoanneum/ippr/configservice/ConfigurationServiceApplication.java
@@ -2,11 +2,13 @@ package at.fhjoanneum.ippr.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationServiceApplication.class, args);
-	}
+  public static void main(final String[] args) {
+    SpringApplication.run(ConfigurationServiceApplication.class, args);
+  }
 }
