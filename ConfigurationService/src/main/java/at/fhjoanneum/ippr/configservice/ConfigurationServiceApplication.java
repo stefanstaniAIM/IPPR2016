8df7af82ddff63ad2386ca3c2ea8e549ca583fc3
Stefan Stani <stefan.stani@gmail.com>
2017-03-06 19:47:32 +0100

    First version of configuration service

---
+++ ConfigurationService/src/main/java/at/fhjoanneum/ippr/configservice/ConfigurationServiceApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+}
