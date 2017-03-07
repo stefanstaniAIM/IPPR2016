412cea9ca49d8c740e26f1720f858e078edf7f29
Stefan Stani <stefan.stani@gmail.com>
2017-03-07 19:53:57 +0100

    Updated config

---
+++ ExternalCommunicator/src/main/java/at/fhjoanneum/ippr/communicator/persistence/TestRunner.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr.communicator.persistence;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements CommandLineRunner {
+
+  @Override
+  public void run(final String... args) throws Exception {}
+
+}
