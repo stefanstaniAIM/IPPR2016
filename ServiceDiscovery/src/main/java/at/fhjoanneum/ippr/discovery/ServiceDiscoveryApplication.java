73fe09f9b646024d37e53b0fe191a6b4e26465e7
Stefan Stani <stefan.stani@gmail.com>
2017-03-10 09:33:06 +0100

    renamed service

---
+++ ServiceDiscovery/src/main/java/at/fhjoanneum/ippr/discovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package at.fhjoanneum.ippr.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(ServiceDiscoveryApplication.class, args);
+  }
+}
