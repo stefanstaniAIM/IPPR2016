3f523f341d8be125f960877063e89c58238fcf4e
stefanstaniAIM <stefan.stani@edu.fh-joanneum.at>
2017-03-13 16:20:18 +0100

    Update DB_setup.sql

--- Setup/DB_setup.sql
+++ Setup/DB_setup.sql
@@ -1,13 +1,3 @@
-CREATE SCHEMA `ippr`;
-COMMIT;
-
-CREATE SCHEMA `ippr_security` ;
-COMMIT;
-
-CREATE SCHEMA `ippr_communicator` ;
-COMMIT;
-
-
 CREATE USER 'ippr'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
 GRANT ALL PRIVILEGES ON * . * TO 'ippr'@'localhost';
 FLUSH PRIVILEGES;
