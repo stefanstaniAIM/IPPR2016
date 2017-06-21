b155e80a9199c42fd61af3705a30d140cf79ca39
Stefan Stani <stefan.stani@edu.fh-joanneum.at>
2017-06-21 18:20:07 +0200

    new service

---
+++ EventLogger/src/main/java/at/fhjoanneum/ippr/EventLoggerApplication.java
@@ -0,0 +1,12 @@
+package at.fhjoanneum.ippr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLoggerApplication.class, args);
+	}
+}
