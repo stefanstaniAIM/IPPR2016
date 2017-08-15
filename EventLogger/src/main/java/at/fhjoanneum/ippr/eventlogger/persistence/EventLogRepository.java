bbb0452327680465e9de4524046168fdda6d06fc
MG <mgeisriegler@gmx.at>
2017-08-15 15:43:20 +0200

    order event log by case_id then by event_id

--- EventLogger/src/main/java/at/fhjoanneum/ippr/eventlogger/persistence/EventLogRepository.java
+++ EventLogger/src/main/java/at/fhjoanneum/ippr/eventlogger/persistence/EventLogRepository.java
@@ -12,6 +12,6 @@ public interface EventLogRepository extends CrudRepository<EventLogEntry, Long> 
     @Query(value = "SELECT * FROM event_log AS log WHERE log.process_model_id = :processModelId AND log.resource = :subject AND EXISTS((SELECT 1 from event_log AS x " +
             "WHERE x.case_id = log.case_id AND x.activity = 'Process Start' AND x.message_type = '' AND x.resource = '' AND x.state = '')) " +
             "AND EXISTS((SELECT 1 from event_log AS y WHERE y.case_id = log.case_id AND y.activity = 'Process End' AND y.message_type = '' AND y.resource = '' AND y.state = ''))" +
-            "ORDER BY log.event_id", nativeQuery = true)
+            "ORDER BY log.case_id, log.event_id", nativeQuery = true)
     public List<EventLogEntry> getEventLogForProcessModelAndSubject(@Param("processModelId") int processModelId, @Param("subject") String subject);
 }
